32069ac9271ab802e127ed218bc87331ea039b89
Christian <christiannguyendinh@yahoo.com>
2017-07-23 20:57:06 -0400

    update scrape script to pull from static url to csv

--- scrape.py
+++ scrape.py
@@ -1,35 +1,61 @@
 import os
 from bs4 import BeautifulSoup
+import requests
 
-# http://na.finalfantasyxiv.com/lodestone/playguide/db/item/?page=1&order=1
+def get_data():
+    html_doc = ""
+    data = ""
 
-html_doc = ""
+    url = 'http://na.finalfantasyxiv.com/lodestone/playguide/db/item/?page=1&order=1'
+    r = requests.get(url)
 
-# Switch to curl later w/ internet
-with open('./test.html', 'r') as file:
-    for line in file:
-        html_doc = html_doc + line
+    html_doc = r.text
 
-soup = BeautifulSoup(html_doc, 'html.parser')
+    '''
+    Offline processing
+    '''
+    # Switch to curl later w/ internet
+    # with open('./test.html', 'r') as file:
+    #     for line in file:
+    #         html_doc = html_doc + line
 
-table = soup.find(class_='db-table').find('tbody')
-for row in table.find_all('tr'):
-    
-    print "----------------"
+    '''
+    Sift thru html and scrape out data
+    '''
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    table = soup.find(class_='db-table').find('tbody')
 
-    name = row.find('a', 'db-table__txt--detail_link').text
-    _types = row.find('span', 'db-table__txt--type').find_all('a')
-    main_type = _types[0].text
-    sub_type = _types[1].text
-    img = row.find('img', 'db-list__item__icon__item_image')['src']
-    _levels = row.find_all('td', 'db-table__body--center')
-    item_level = _levels[0].text
-    req_level = _levels[1].text
+    for row in table.find_all('tr'):
+        
+        name = row.find('a', 'db-table__txt--detail_link').text
+        _types = row.find('span', 'db-table__txt--type').find_all('a')
+        main_type = _types[0].text
+        sub_type = _types[1].text
+        img = row.find('img', 'db-list__item__icon__item_image')['src']
+        _levels = row.find_all('td', 'db-table__body--center')
+        item_level = _levels[0].text if _levels[0].text != '-' else '0'
+        req_level = _levels[1].text if _levels[1].text != '-' else '0'
 
-    print name
-    print '%s > %s' % (main_type, sub_type)
-    print img
-    print item_level
-    print req_level
+        '''
+        Print Results to console
+        '''
+        # data = data + "----------------\n"
+        # data = data + name + "\n"
+        # data = data + '%s > %s\n' % (main_type, sub_type)
+        # data = data + img + "\n"
+        # data = data + item_level + "\n"
+        # data = data + req_level + "\n"
+        # data = data + "----------------\n"
 
-    print "----------------"
+        '''
+        Format results into csv for write to file
+        '''
+        data = data + '\n' + name + ',' + main_type + ',' + sub_type + ',' + img + ',' + item_level + ',' +  req_level 
+
+    # print data
+
+    with open('item_data.csv', 'a') as data_file:
+        data_file.write(data)
+
+
+get_data()
