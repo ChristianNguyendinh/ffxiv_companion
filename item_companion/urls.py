3453eec59a101202f8c34d078b1f0fd7ec158a79
Christian <christiannguyendinh@yahoo.com>
2017-07-24 14:48:04 -0400

    endpoint to retrieve names of all items

--- item_companion/urls.py
+++ item_companion/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home),
+	url(r'^all/$', views.all_items),
 ]
--- item_companion/views.py
+++ item_companion/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Items
 
 def home(req):
 	return render(req, 'item_companion/home.html')
+
+'''
+Return the name of all items
+'''
+def all_items(req):
+	items = [ str(item.name) for item in Items.objects.all() ]
+	return JsonResponse({'items' : items})
